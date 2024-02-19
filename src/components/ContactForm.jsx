9ae2377fd1eeb0fff1bef428b6b6fad765d5a30d
Kevin <tomfge09@yahoo.ca>
2024-02-19 00:21:04 -0700

    Initial files has been updated

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,29 @@
+
+const ContactForm = () => {
+  return (
+    <div className="form-container">
+        <div className="form-title">
+            <h2>Contact Us</h2>
+        </div>
+
+        <form className="form" action="emai">
+            <div className="fname-input">
+                <input className="fname" type="text" name="fname" placeholder="Full Name"/>
+            </div>
+            <div className="email-input">
+                <input className="email" type="email" name="email" placeholder="Enter Your Email"/>
+            </div>
+            <div className="message-input">
+                <textarea className="text-message" name="message" placeholder="Message"></textarea>
+            </div>
+            <div className="btn-container">
+                <button className="form-btn">Submit</button>
+            </div>
+
+        </form>
+
+    </div>
+  )
+}
+
+export default ContactForm
--- src/pages/contact/ContactPage.jsx
+++ src/pages/contact/ContactPage.jsx
@@ -1,3 +1,4 @@
+import ContactForm from "../../components/ContactForm"
 import Footer from "../../components/footer/Footer"
 import './contact.css'
 const ContactPage = () => {
@@ -7,7 +8,43 @@ const ContactPage = () => {
         <h1 className='about-hero-title'>Contact Us</h1>
       </section>
       <div className="contact-container">
-        
+        <div className="contact-content">
+          {/* CONTACT MAP */}
+          <div className="contact-map">
+            <iframe 
+              className="google-map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2402.850641394132!2d-113.36976342271596!3d52.969101202882136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53754e3ec9dc9ca5%3A0x348d82b68ebdb998!2sLifegate%20Community%20Church!5e0!3m2!1sen!2sca!4v1708322480177!5m2!1sen!2sca" 
+              loading="lazy" 
+              referrerPolicy="no-referrer-when-downgrade"
+              >
+            </iframe>
+          </div>
+          {/* CONTACT INFO */}
+          <div className="contact-info">
+            <h2 className="contact-title">Contact Info</h2>
+            <div className="contact-details">
+              <div className="address">
+                <p className="contact-text">Address:</p>
+                <p>
+                  4708 50 Avenue <br />
+                  Wetaskiwin, Alberta T9A 0R7
+                </p>
+              </div>
+              <div className="email-info">
+                <p className="contact-text">Email:</p>
+                <p>ccwetaskiwin@gmail.com</p>
+              </div>
+              <div className="phone">
+                <p className="contact-text">Phone:</p>
+                <p>(403) 913-4713</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="contact-form">
+        <ContactForm/>
       </div>
       <Footer/>
     </div>
