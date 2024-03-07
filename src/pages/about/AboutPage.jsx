11939215b8726d633f6b96fbe418cf09fe26468d
Kevin <tomfge09@yahoo.ca>
2024-03-06 20:33:11 -0700

    Initial About Page has been updated

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -11,6 +11,7 @@ import imageSix from "./images/Family Day 2024.jpg"
 import imageSeven from "./images/camping.png"
 import imageEight from "./images//camping 2022.jpg"
 import logoImg from '../../images/logo 2.webp'
+import aboutImg from '../../images/about-img.png'
 
 const imageGallery = [
   {imgUrl: imageOne},
@@ -35,18 +36,34 @@ const AboutPage = () => {
               <div className="about-details">
                   <h2 className="about-title">About Our Church</h2>
                   <p className="about-text">
-                    At Church of Christ, we are more than just a gathering of individuals; 
-                    we are a community united by the common goal of worshipping and serving God. 
-                    We are committed to create an environment where believers can deepen our relationship with God, 
-                    express our love through worship, and serve others with a spirit of humility.
+                  Wetaskiwin Church of Christ is an elder-governed community. 
+                  We are functioning with multiple leadership and Elders <span style={{color: '#b68911'}}>(our spiritual shepherding team)</span> are designated as key leaders of the church as attested in the New Testament 
+                  <span style={{color: '#b68911'}}> (1 Timothy 3:1-7; Acts 14:23; Acts 20:28,  I Timothy 5:17-18 ; I Peter 5:1-4)</span>. 
+                  The elders are called to lead, to add values and to live exemplary lives of faith to the church and the community. 
                   </p>
+                 
               </div>
               <div className="about-img-container">
                   <img className='about-img' src={headImage} alt="CCW Image" />
                   {/* <div className="box"></div> */}
               </div>
           </div>
-    </div>
+          <div className='about-container'>
+                <div className="about-img-container">
+                  <img className='about-img' src={aboutImg} alt="CCW Image" />
+                  {/* <div className="box"></div> */}
+                </div>
+                <p className="about-text">
+                      AT CCW Anyone can be an elder when someone meets the qualities and desires the work. 
+                      The appointment process can happen anytime during the year.  
+                      Because we do not set a number of spots to fill in the leadership position of Elders, 
+                      we are open to anyone who on the basis of biblical qualities and desires would like to serve. 
+                      We look for members who are already serving in the ministry and we will ask the individual if they have a desire to be an elder <span style={{color: '#b68911'}}>(I Timothy 3:1)</span>. 
+                      This process involves a questionnaire and an interview, Followed by the congregation approval. Then culminated by an ordination to the work of an elder.
+                </p>
+
+          </div>
+        </div>
 
     {/* PRINCIPLES SECTION */}
     <section className='principles-section'>
