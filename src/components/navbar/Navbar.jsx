3d7f596d54177fccb34a2ff8c44c4ad9d14d3326
Kevin <tomfge09@yahoo.ca>
2024-07-28 23:20:06 -0600

    Nav links has been updated and added use location

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link , useLocation} from 'react-router-dom';
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoCloseSharp } from "react-icons/io5";
 import { useState } from 'react';
 import './navbar.css';
 import logo from '../../images/logo/logo.png';
+import { useEffect } from 'react';
 
 const Navbar = () => {
   const [navToggle, setNavToggle] = useState(false)
-
+  const [activeItem, setActiveItem] = useState(null);
+  const location = useLocation();
   const closeMobile = () =>{
     scrollToTop()
     setNavToggle(false)
@@ -18,6 +20,11 @@ const Navbar = () => {
       behavior: "smooth" // Optional: Adds smooth scrolling behavior
     });
   };
+  useEffect(() => {
+    // Extract the last part of the pathname as the activeItem
+    const lastPath = location.pathname.split('/').pop();
+    setActiveItem(lastPath || 'item1'); // Default to 'item1' if no path
+  }, [location.pathname]);
 
   return (
     <nav className='nav-container'>
@@ -29,22 +36,42 @@ const Navbar = () => {
         </div>
         <ul className={`nav-item ${navToggle ? 'active' : ''}`}>
           <li className='nav-list'>
-            <Link className='nav-link' to="/" onClick={closeMobile}>Home</Link>
+            <Link 
+            className={activeItem === 'item1' ? 'active' : 'nav-link'}
+            to="/" 
+            onClick={closeMobile}>
+            Home
+            </Link>
           </li>
           <li className='nav-list'>
-            <Link className='nav-link' to="/about" onClick={closeMobile}>About</Link>
+            <Link 
+            className={activeItem === 'about' ? 'active' : 'nav-link'}
+            to="/about" 
+            onClick={closeMobile}>About</Link>
           </li>
           <li className='nav-list'>
-            <Link className='nav-link' to="/events" onClick={closeMobile}>Events</Link>
+            <Link 
+            className={activeItem === 'events' ? 'active' : 'nav-link'}
+            to="/events" 
+            onClick={closeMobile}>Events</Link>
           </li>
           <li className='nav-list'>
-            <Link className='nav-link' to="/messages" onClick={closeMobile}>Messages</Link>
+            <Link 
+            className={activeItem === 'messages' ? 'active' : 'nav-link'}
+            to="/messages" 
+            onClick={closeMobile}>Messages</Link>
           </li>
           <li className='nav-list'>
-            <Link className='nav-link' to="/give" onClick={closeMobile}>Give</Link>
+            <Link 
+            className={activeItem === 'give' ? 'active' : 'nav-link'}
+            to="/give" 
+            onClick={closeMobile}>Give</Link>
           </li>
           <li className='nav-list'>
-            <Link className='nav-link' to="/contact" onClick={closeMobile}>Contact Us</Link>
+            <Link 
+            className={activeItem === 'contact' ? 'active' : 'nav-link'}
+            to="/contact" 
+            onClick={closeMobile}>Contact Us</Link>
           </li>
         </ul>
         <div className="burger-menu" onClick={() => setNavToggle(!navToggle)}>
