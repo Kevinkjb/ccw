2f524a1512f13979a8d8b563ccd0e3f9618b392d
Kevin <tomfge09@yahoo.ca>
2025-04-02 18:31:29 -0600

    Initial files has been updated

--- src/components/components-data/data.js
+++ src/components/components-data/data.js
@@ -10,7 +10,7 @@ const latestMessage = [
         messageBy: 'Bro. Bong Aguiao',
         datePosted: 'March 30, 2025',
         imageSlide: 'https://www.canva.com/design/DAGjJIk_Nfo/IXtayrbpi6fZy6rzQLaxQw/view?utm_content=DAGjJIk_Nfo&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h73cf7d00fa',
-        description: `But you, dear friends, must build each other up in your most holy faith,How do we do build each other up? By his Holy Word, by knowing more of Jesus Christ.`
+        description: `But you, dear friends, must build each other up in your most holy faith, How do we do build each other up? By his Holy Word, by knowing more of Jesus Christ.`
     }
 ]
 
