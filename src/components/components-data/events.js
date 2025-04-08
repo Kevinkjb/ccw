5d36a59de4f9f6c6f7c77260ca036a21e2793a5b
Kevin <tomfge09@yahoo.ca>
2025-04-08 16:06:07 -0600

    Initial files has been updated

--- src/components/components-data/events.js
+++ src/components/components-data/events.js
@@ -6,20 +6,29 @@
 // import valetines from '../../images/events/Valentines.jpg'
 // import anniversary from '../../images/events/anniversary.jpg'
 // import camping from '../../images/events/Couple’s.png'
-import kidsFunDay from '../../images/events/Kids fun day.jpg'
+// import kidsFunDay from '../../images/events/Kids fun day.jpg'
 import churchCamp from '../../images/events/Camp.jpg'
+import youthMovie from '../../images/events/Good Friday Movie Night.pdf.png'
 
 
 const events = [
-    // {
-    //     id: 0,
-    //     imgUrl: camping,
-    //     eventMonths: 'Aug',
-    //     eventDays: '02',
-    //     eventTitle: "Young Couple's Camp",
-    //     description: 'Learn and grow to love the Lord. ',
-    //     time: 'Friday 5:00 PM'
-    // },
+    {
+        id: 0,
+        imgUrl: youthMovie,
+        eventMonths: 'Apr',
+        eventDays: '18',
+        eventTitle: "Good Friday Movie Night",
+        description: `
+            All youths invited
+            Free movie screening and popcorn
+            Concessions will be available for purchase (cash only)
+
+            April 18, 2025 @ 5:30 PM
+            Movie starts @ 6:00 PM
+            4708 50 Avenue Wetaskiwin, AB
+        `,
+        time: 'Friday @ 5:30PM'
+    },
     // {
     //     id: 1,
     //     imgUrl: anniversary,
@@ -29,17 +38,17 @@ const events = [
     //     description: 'We look back on how God has blessed us and we look forward, united, with confident hope because we trust in Him who lead us.',
     //     time: 'Sunday 10:00 AM'
     // },
-    {
-        id: 0,
-        imgUrl: kidsFunDay,
-        eventMonths: 'Mar',
-        eventDays: '29',
-        eventTitle: "Kids Fun Day",
-        description: `
-        Love like Jesus 1 John 3:16
-        `,
-        time: '9:30AM - 11:30AM'
-    },
+    // {
+    //     id: 0,
+    //     imgUrl: kidsFunDay,
+    //     eventMonths: 'Mar',
+    //     eventDays: '29',
+    //     eventTitle: "Kids Fun Day",
+    //     description: `
+    //     Love like Jesus 1 John 3:16
+    //     `,
+    //     time: '9:30AM - 11:30AM'
+    // },
     {
         id: 1,
         imgUrl: churchCamp,
