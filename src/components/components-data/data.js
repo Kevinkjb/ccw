5bf34c9d7167957148dee611fc5cba5856e78e4a
Kevin <tomfge09@yahoo.ca>
2025-06-27 18:43:45 -0600

    Initial files has been updated

--- src/components/components-data/data.js
+++ src/components/components-data/data.js
@@ -1,16 +1,16 @@
 
 
-import spiritOfExcellence from '../../images/message/05.18.2025 - Spirit of Excellence Joseph - Bro. Marvin Risto.png'
+import excellenceStewardship from '../../images/message/06.22.2025 - Excellence in Stewardship - Bro. Boni Somook.png'
 
 const latestMessage = [
     {
-        messageTitle: "Spirit of Excellence",
-        videoUrl: "https://www.youtube.com/watch?v=MOWugjkAv3E",
-        imgUrl: spiritOfExcellence,
-        messageBy: 'Bro. Marvin Risto',
-        datePosted: 'May 18, 2025',
-        imageSlide: 'https://www.canva.com/design/DAGnuf8yhzU/grHZL5S69VpNPOMYPELUdA/view?utm_content=DAGnuf8yhzU&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hae291f71b4',
-        description: `“So then, it was not you who sent me here, but God. He made me father to Pharaoh, lord of his entire household and ruler of all Egypt.`
+        messageTitle: "Excellence in Stewardship",
+        videoUrl: "https://www.youtube.com/watch?v=rM9WlUnNg3c",
+        imgUrl: excellenceStewardship,
+        messageBy: 'Bro. Boni Somook',
+        datePosted: 'June 22, 2025',
+        imageSlide: 'https://www.canva.com/design/DAGrA7nGYRA/KogGgL8C7cKfCK8yINNa6Q/view?utm_content=DAGrA7nGYRA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h3ee4ef5051',
+        description: `We are stewards of One who is EXCELLENT in every way, every time, and in anything.`
     }
 ]
 
--- src/pages/MessagesPage.jsx
+++ src/pages/MessagesPage.jsx
@@ -11,6 +11,7 @@ import toOnlyGod from '../images/message/To the only God.png'
 import commitedExcellence from '../images/message/04.13.2025 - Commitment to Excellence - Bro. Ted Matira.png'
 import reflection from '../images/message/Easter Reflection.png'
 import excellentSpirit from '../images/message/April 27, 2025 Message.png'
+import spiritOfExcellence from '../images/message/05.18.2025 - Spirit of Excellence Joseph - Bro. Marvin Risto.png'
 import excellentStewardship from '../images/message/05.04.2025 - Excellence in Stewardship - Bro. Boni Somook.png'
 // import InexpressibleJoy from '../images/message/Inexpressible Joy.pdf.png'
 // import GoTell from '../images/message/Go and Tell.pptx.png'
@@ -47,6 +48,15 @@ import excellentStewardship from '../images/message/05.04.2025 - Excellence in S
 
 
 const allMessage = [
+  {
+      messageTitle: "Spirit of Excellence",
+      videoUrl: "https://www.youtube.com/watch?v=MOWugjkAv3E",
+      imgUrl: spiritOfExcellence,
+      messageBy: 'Bro. Marvin Risto',
+      datePosted: 'May 18, 2025',
+      imageSlide: 'https://www.canva.com/design/DAGnuf8yhzU/grHZL5S69VpNPOMYPELUdA/view?utm_content=DAGnuf8yhzU&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hae291f71b4',
+      description: `“So then, it was not you who sent me here, but God. He made me father to Pharaoh, lord of his entire household and ruler of all Egypt.`
+  },
   {
           messageTitle: "Excellence in Stewardship",
           videoUrl: "https://www.youtube.com/watch?v=QpAyjnUMBaA",
