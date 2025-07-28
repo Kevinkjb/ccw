3cdf0bb73cbe89554fee0347f805d89941a7c111
Kevin <tomfge09@yahoo.ca>
2025-07-28 11:04:05 -0600

    Initial files has been updated

--- src/components/components-data/data.js
+++ src/components/components-data/data.js
@@ -10,7 +10,7 @@ const latestMessage = [
         messageBy: 'Bro. Ted Matira',
         datePosted: 'July 27, 2025',
         imageSlide: 'https://www.canva.com/design/DAGuMwK9g0A/Cr0g9fhY1AjlIPqxmDFhTQ/view?utm_content=DAGuMwK9g0A&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h055efca09e',
-        description: `6 you will be for me a kingdom of priests and a holy nation.’ These are the words you are to speak to the Israelites.”`
+        description: `You will be for me a kingdom of priests and a holy nation.’ These are the words you are to speak to the Israelites.”`
     }
 ]
 
