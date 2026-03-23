8f4aba27d6f5e9ec16fdeba6651fafa7e1505bb9
Kevin <tomfge09@yahoo.ca>
2026-03-23 17:05:53 -0600

    Initial files has been updated

--- src/components/components-data/data.js
+++ src/components/components-data/data.js
@@ -1,16 +1,16 @@
 
 
-import EmbracingKingdom from '../../images/luke-series/Embracing kingdom.png'
+import BlindJudgement from '../../images/luke-series/Blind Judgement.png'
 
 const latestMessage = [
    {
-          messageTitle: "Embracing Kingdom",
-          videoUrl: "https://www.youtube.com/watch?v=1A8qEpo4fZY",
-          imgUrl: EmbracingKingdom,
-          messageBy: 'Bro. Marvin Risto',
-          datePosted: 'March 15, 2026',
-          imageSlide: 'https://www.canva.com/design/DAHD5kZtQwk/gMvWPpTVCknECORAjMUAIg/view?utm_content=DAHD5kZtQwk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h454bff72fa',
-          description: `27 “But to you who are listening I say: Love your enemies, do good to those who hate you, 28 bless those who curse you, pray for those who mistreat you. 29 If someone slaps you on one cheek, turn to them the other also. If someone takes your coat, do not withhold your shirt from them. `
+          messageTitle: "Blind Judgement",
+          videoUrl: "https://www.youtube.com/watch?v=nwvTOjUCgtw",
+          imgUrl: BlindJudgement,
+          messageBy: 'Bro. Ted Matira',
+          datePosted: 'March 22, 2026',
+          imageSlide: 'https://www.canva.com/design/DAHElxbQ1vI/zh2fvz2Y01YzCvHwZwjjeA/view?utm_content=DAHElxbQ1vI&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h5766609c4c',
+          description: `37 “Do not judge, and you will not be judged. Do not condemn, and you will not be condemned. Forgive, and you will be forgiven. 38 Give, and it will be given to you. A good measure, pressed down, shaken together and running over, will be poured into your lap. For with the measure you use, it will be measured to you.”`
       }
 ]
 
--- src/pages/MessagesPage.jsx
+++ src/pages/MessagesPage.jsx
@@ -24,7 +24,7 @@ import NextTime from '../images/luke-series/Next time.png'
 import TheNextTable from '../images/luke-series/TheNextTable.png'
 import LeavingOldBehind from '../images/luke-series/LeavingOldBehind.png'
 import LordOfSabbath from '../images/luke-series/Jesus, Lord of the Sabbath.png'
-// import EmbracingKingdom from '../images/luke-series/Embracing kingdom.png'
+import EmbracingKingdom from '../images/luke-series/Embracing kingdom.png'
 import NotAbandoned from '../images/luke-series/NotAbandoned.png'
 import UpIsDown from '../images/luke-series/UpIsDown.png'
 //----------------------------------------------------------------------------
@@ -96,8 +96,15 @@ const allMessage = [
   
        
   // Luke Series Sermon
- 
-   
+          {
+              messageTitle: "Embracing Kingdom",
+              videoUrl: "https://www.youtube.com/watch?v=1A8qEpo4fZY",
+              imgUrl: EmbracingKingdom,
+              messageBy: 'Bro. Marvin Risto',
+              datePosted: 'March 15, 2026',
+              imageSlide: 'https://www.canva.com/design/DAHD5kZtQwk/gMvWPpTVCknECORAjMUAIg/view?utm_content=DAHD5kZtQwk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h454bff72fa',
+              description: `27 “But to you who are listening I say: Love your enemies, do good to those who hate you, 28 bless those who curse you, pray for those who mistreat you. 29 If someone slaps you on one cheek, turn to them the other also. If someone takes your coat, do not withhold your shirt from them. `
+          },
           {
               messageTitle: "Up is Down",
               videoUrl: "https://www.youtube.com/watch?v=aFzZqQFj6CQ",
