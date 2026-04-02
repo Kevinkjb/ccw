3350c0a6d3fcbf5668f0f91b6b957c9b8183e73f
Kevin <tomfge09@yahoo.ca>
2026-04-01 18:45:09 -0600

    Initial files has been updated

--- src/components/components-data/data.js
+++ src/components/components-data/data.js
@@ -1,16 +1,16 @@
 
 
-import BlindJudgement from '../../images/luke-series/Blind Judgement.png'
+import KnowingTheTree from '../../images/luke-series/What kind of tree is this.png'
 
 const latestMessage = [
    {
-          messageTitle: "Blind Judgement",
-          videoUrl: "https://www.youtube.com/watch?v=nwvTOjUCgtw",
-          imgUrl: BlindJudgement,
-          messageBy: 'Bro. Ted Matira',
-          datePosted: 'March 22, 2026',
-          imageSlide: 'https://www.canva.com/design/DAHElxbQ1vI/zh2fvz2Y01YzCvHwZwjjeA/view?utm_content=DAHElxbQ1vI&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h5766609c4c',
-          description: `37 “Do not judge, and you will not be judged. Do not condemn, and you will not be condemned. Forgive, and you will be forgiven. 38 Give, and it will be given to you. A good measure, pressed down, shaken together and running over, will be poured into your lap. For with the measure you use, it will be measured to you.”`
+          messageTitle: "Heartbreaking Challenge",
+          videoUrl: "https://www.youtube.com/watch?v=h3yYLLW_zm0&t=1399s",
+          imgUrl: KnowingTheTree,
+          messageBy: 'Bro. Kevin Baguioso',
+          datePosted: 'March 29, 2026',
+          imageSlide: 'https://canva.link/n7jmdj68hn32njz',
+          description: `'45A good person produces good things from the treasury of a good heart, and an evil person produces evil things from the treasury of an evil heart. What you say flows from what is in your heart.'`
       }
 ]
 
--- src/pages/MessagesPage.jsx
+++ src/pages/MessagesPage.jsx
@@ -18,6 +18,7 @@ import LoveInAnyLanguage from '../images/series-break/LoveInAnyLanguage.png'
 
 
 // LUKE SERIES--------------------------------------------------------------------------
+import BlindJudgement from '../images/luke-series/Blind Judgement.png'
 import NextMove from '../images/luke-series/Next Move.png'
 import whatsNext from '../images/luke-series/WHAT’S NEXTS.png'
 import NextTime from '../images/luke-series/Next time.png'
@@ -96,6 +97,15 @@ const allMessage = [
   
        
   // Luke Series Sermon
+  {
+            messageTitle: "Blind Judgement",
+            videoUrl: "https://www.youtube.com/watch?v=nwvTOjUCgtw",
+            imgUrl: BlindJudgement,
+            messageBy: 'Bro. Ted Matira',
+            datePosted: 'March 22, 2026',
+            imageSlide: 'https://www.canva.com/design/DAHElxbQ1vI/zh2fvz2Y01YzCvHwZwjjeA/view?utm_content=DAHElxbQ1vI&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h5766609c4c',
+            description: `37 “Do not judge, and you will not be judged. Do not condemn, and you will not be condemned. Forgive, and you will be forgiven. 38 Give, and it will be given to you. A good measure, pressed down, shaken together and running over, will be poured into your lap. For with the measure you use, it will be measured to you.”`
+        },
           {
               messageTitle: "Embracing Kingdom",
               videoUrl: "https://www.youtube.com/watch?v=1A8qEpo4fZY",
