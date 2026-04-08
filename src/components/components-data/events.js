b9f237686022d04e5147fb93728ae6c338002045
Kevin <tomfge09@yahoo.ca>
2026-04-07 20:48:41 -0600

    Initial files has been updated

--- src/components/components-data/events.js
+++ src/components/components-data/events.js
@@ -1,6 +1,7 @@
 // import womensRetreat from '../../images/events/Retreat2025.png'
 // import anniversary from '../../images/events/Church Anniversary.jpg'
 import familyCamp from '../../images/events/2026-events/Family Camp.png'
+import bibleStudy from '../../images/events/2026-events/Welcome Winter 2025.png'
 
 const events = [
 
@@ -14,9 +15,20 @@ const events = [
         description: `
         Where faith breaks comfort & God makes a way..
         Come Join Us!
-
-
         `,
+        
+
+    },
+       {
+        id: 1,
+        imgUrl: bibleStudy,
+        eventMonths: 'May',
+        eventDays: '16',
+        eventTitle: "Ephesians Study",
+        description: `
+        We invite you to dive into the transformative words of the Book of Ephesians with us.
+        `,
+        
 
     }
        
